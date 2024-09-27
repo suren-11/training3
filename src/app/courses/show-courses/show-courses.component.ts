29eaab21c8fc3260c76bc51dfb9bb42b9edf0c80
suren-11 <presanth11@gmail.com>
2024-09-27 15:57:25 +0530

    implemented ui for add course and show courses with dummy value

--- src/app/courses/show-courses/show-courses.component.ts
+++ src/app/courses/show-courses/show-courses.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrl: './show-courses.component.scss'
 })
 export class ShowCoursesComponent {
+  courses = [
+    {
+      'id': 1,
+      'name': 'Java',
+      'fees': '100000',
+      'duration': '4 Months',
+    },
+    {
+      'id': 2,
+      'name': 'C#',
+      'fees': '200000',
+      'duration': '5 Months',
+    },
+    {
+      'id': 3,
+      'name': 'Angular',
+      'fees': '300000',
+      'duration': '3 Months',
+    },
+  ];
 
 }
