bdeba1f819a0fb80289c5c4ab85f110f9be5e908
suren-11 <presanth11@gmail.com>
2024-09-27 11:35:21 +0530

    developed nav bar in private base

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { PublicBaseComponent } from './public-base/public-base.component';
 import { LoginComponent } from './login/login.component';
+import { PrivateBaseComponent } from './private-base/private-base.component';
 
 const routes: Routes = [
-  {path:'login', component: PublicBaseComponent, children:[{path:'', component: LoginComponent}]},
-  {path:'**', redirectTo: '/login', pathMatch:'full'},
-  {path:'', redirectTo:'/login', pathMatch:'full'},
+  { path: 'login', component: PublicBaseComponent, children: [{ path: '', component: LoginComponent }] },
+  { path: 'dashboard', component: PrivateBaseComponent },
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: '**', redirectTo: '/login'},
 ];
 
 @NgModule({
