a11bba7f830a2cf119be50dfa628b2b28167070c
suren-11 <presanth11@gmail.com>
2024-09-27 10:14:51 +0530

    generated a public and private base components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PublicBaseComponent } from './public-base/public-base.component';
+import { PrivateBaseComponent } from './private-base/private-base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PublicBaseComponent,
+    PrivateBaseComponent
   ],
   imports: [
     BrowserModule,
