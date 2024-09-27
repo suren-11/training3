d5d456b728da83fb52af48f4e9e987ab81ff13dd
suren-11 <presanth11@gmail.com>
2024-09-27 10:20:56 +0530

    generated add course, edit course, show courses components

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddCourseComponent } from './add-course/add-course.component';
+import { ShowCoursesComponent } from './show-courses/show-courses.component';
+import { EditCourseComponent } from './edit-course/edit-course.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddCourseComponent,
+    ShowCoursesComponent,
+    EditCourseComponent
+  ],
   imports: [
     CommonModule
   ]
