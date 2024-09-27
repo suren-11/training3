e90d1e16ef345522975e5e45316638fdc9873792
suren-11 <presanth11@gmail.com>
2024-09-27 10:36:57 +0530

    implement the initial routes to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PublicBaseComponent } from './public-base/public-base.component';
+import { LoginComponent } from './login/login.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'login', component: PublicBaseComponent, children:[{path:'', component: LoginComponent}]},
+  {path:'**', redirectTo: '/login', pathMatch:'full'},
+  {path:'', redirectTo:'/login', pathMatch:'full'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
