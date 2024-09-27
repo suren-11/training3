74fda16f542f856b7433d7314e9e69e643b204bd
suren-11 <presanth11@gmail.com>
2024-09-27 15:31:20 +0530

    developed show students with dummy values

--- src/app/student/show-students/show-students.component.ts
+++ src/app/student/show-students/show-students.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class ShowStudentsComponent {
 
+  students = [
+    {
+      'id': 1,
+      'name': 'suren',
+      'address': 'colombo',
+      'dob': '11.09.1992',
+      'course': 'java',
+      'tel': '0752314569'
+    },
+    {
+      'id': 2,
+      'name': 'kamal',
+      'address': 'jaffna',
+      'dob': '11.09.1992',
+      'course': 'C#',
+      'tel': '0745614577'
+    },
+    {
+      'id': 3,
+      'name': 'bandula',
+      'address': 'kandy',
+      'dob': '11.09.1994',
+      'course': 'Angular',
+      'tel': '0745612486'
+    },
+  ];
+
 }
