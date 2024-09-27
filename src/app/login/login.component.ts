534aa1af30fc6289b741c08950ca712c26c754f3
suren-11 <presanth11@gmail.com>
2024-09-27 16:34:18 +0530

    add login method with dummy values

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
@@ -7,4 +8,16 @@ import { Component } from '@angular/core';
 })
 export class LoginComponent {
 
+  constructor(private router: Router) { }
+  email: string = '';
+  password: string = '';
+
+  login(email: string, password: string) {
+
+    if (email === 'admin@gmail.com' && password === '123456') {
+      this.router.navigate(['/dashboard']);
+    } else {
+      console.log('Invalid credentials');
+    }
+  }
 }
