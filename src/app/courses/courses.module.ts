34f23e510afeccfec6e5ebded6c40bf638315ef3
suren-11 <presanth11@gmail.com>
2024-09-27 16:11:33 +0530

    implement the edit course ui

--- src/app/courses/courses.module.ts
+++ src/app/courses/courses.module.ts
@@ -7,7 +7,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   { path: 'add-course', component: AddCourseComponent },
-  { path: 'edit-course', component: EditCourseComponent },
+  { path: 'edit-course/:id', component: EditCourseComponent },
   { path: 'show-courses', component: ShowCoursesComponent },
 ];
 
