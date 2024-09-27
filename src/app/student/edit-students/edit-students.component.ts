596e3869a179457fa5bedb56b60828031c23dcf4
suren-11 <presanth11@gmail.com>
2024-09-27 10:18:51 +0530

    generated add students, show students, edit students components

---
+++ src/app/student/edit-students/edit-students.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-students',
+  templateUrl: './edit-students.component.html',
+  styleUrl: './edit-students.component.scss'
+})
+export class EditStudentsComponent {
+
+}
--- src/app/student/student.module.ts
+++ src/app/student/student.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { AddStudentComponent } from './add-student/add-student.component';
+import { ShowStudentsComponent } from './show-students/show-students.component';
+import { EditStudentsComponent } from './edit-students/edit-students.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    AddStudentComponent,
+    ShowStudentsComponent,
+    EditStudentsComponent
+  ],
   imports: [
     CommonModule
   ]
