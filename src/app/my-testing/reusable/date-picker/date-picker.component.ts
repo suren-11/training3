2377cadb656b387e7d9ce1a056a2015eab53c615
suren-11 <presanth11@gmail.com>
2024-10-03 13:05:13 +0530

    defined the error message in date picker component

--- src/app/my-testing/reusable/date-picker/date-picker.component.ts
+++ src/app/my-testing/reusable/date-picker/date-picker.component.ts
@@ -6,13 +6,14 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './date-picker.component.html',
   styleUrl: './date-picker.component.scss'
 })
-export class DatePickerComponent{
+export class DatePickerComponent {
 
-  dateForm :FormGroup;
+  dateForm: FormGroup;
 
   @Output() dateChange = new EventEmitter<string>();
+  errorMessage: boolean  = false;
 
-  constructor(private fb:FormBuilder) {
+  constructor(private fb: FormBuilder) {
     this.dateForm = this.fb.group({
       day: ['', [Validators.required, Validators.maxLength(2), Validators.min(1), Validators.max(31)]],
       month: ['', [Validators.required, Validators.maxLength(2), Validators.min(1), Validators.max(12)]],
@@ -22,11 +23,32 @@ export class DatePickerComponent{
 
   onDateChange(): void {
     if (this.dateForm.valid) {
-     const formattedDate = `${this.dateForm.get('day')?.value}-${this.dateForm.get('month')?.value}-${this.dateForm.get('year')?.value}`;
-      this.dateChange.emit(formattedDate);
-    }else{
-      this.dateChange.emit('Invalid date')
+      const formattedDate = `${this.dateForm.get('day')?.value}-${this.dateForm.get('month')?.value}-${this.dateForm.get('year')?.value}`;
+      const parsedDate = this.formatDate(formattedDate);
+      
+      if (parsedDate) {
+        this.errorMessage = false;
+        this.dateChange.emit(formattedDate);
+      } else {
+        this.errorMessage = true;
+      }
+    } else {
+      this.errorMessage = true;
     }
+  }
+
+  formatDate(date: string) {
+    if (!date) return false;
+
+    const [day, month, year] = date.split('-').map(Number);
+    
+    const parsedDate = new Date(year, month - 1, day);
+
+    if (parsedDate && parsedDate.getDate() === day && parsedDate.getMonth() === month - 1 && parsedDate.getFullYear() === year) {
+      return true;
+    }
+
+    return false; 
   }
 
 }
--- src/app/my-testing/reusable/reusable-parent/reusable-parent.component.ts
+++ src/app/my-testing/reusable/reusable-parent/reusable-parent.component.ts
@@ -7,19 +7,9 @@ import { Component } from '@angular/core';
 })
 export class ReusableParentComponent {
   selectedDate!: string;
-  isValidDate: boolean = true;
 
   onDateChange(date: string): void {
     this.selectedDate = date;
-    this.isValidDate = this.validateDate(date);
   }
 
-  validateDate(date: string): boolean {
-    const [day, month, year] = date.split('-').map(Number);
-    const parsedDate = new Date(year, month - 1, day);
-    
-    return parsedDate.getFullYear() === year &&
-           parsedDate.getMonth() === month - 1 &&
-           parsedDate.getDate() === day;
-  }
 }
